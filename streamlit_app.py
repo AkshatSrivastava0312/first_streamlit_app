10453c954d38ee4cddb0a741f1cd62afbf6d7c0a
Akshat Srivastava <akshat.srivastava0312@gmail.com>
2023-10-16 12:23:17 +0530

    Update streamlit_app.py
    Extended the code in the main file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My First Streamlit Application')
+
+streamlit.header('Following are the names of some programming languages')
+
+streamlit.text('Java')
+streamlit.text('Python')
+streamlit.text('C++')
+streamlit.text('C')
+streamlit.text('JavaScript')

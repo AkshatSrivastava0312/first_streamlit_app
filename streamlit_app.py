ea6d3bdfe7b5619b81eed238c260d6d7786c7764
Akshat Srivastava <akshat.srivastava0312@gmail.com>
2023-10-16 12:26:17 +0530

    Update streamlit_app.py
    Added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,8 @@
 import streamlit
 
-streamlit.title('My First Streamlit Application')
+streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Following are the names of some programming languages')
-
-streamlit.text('Java')
-streamlit.text('Python')
-streamlit.text('C++')
-streamlit.text('C')
-streamlit.text('JavaScript')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
